ee2afee2622d6bf6a74b3f13dd5637002386d597
Jonathan Norman <jnorman2123@gmail.com>
2019-10-26 13:28:03 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,50 @@
+class Song
+  # extend Concerns::Findable
+  attr_accessor :name, :artist, :genre
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist=(artist)
+    self.genre=(genre)
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    self.new(name)
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self) if self.artist
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.add_song(self) if self.genre
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+end
